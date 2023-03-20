b21bcf85e5670b5526520b01adecba78d841aaca
Michał Smolik <smolik.it@gmail.com>
2023-03-20 21:20:37 +0100

    update admin project

--- apps/admin/src/app.tsx
+++ apps/admin/src/app.tsx
@@ -1,5 +1,15 @@
 import React from 'react';
+import { isEven } from 'is-even';
+import { Container } from '@/app.styled';
+
+const getIsEventText = (num: number): string => (isEven(num) ? 'Yes' : 'No');
 
 export const App = () => {
-  return <h1>Admin</h1>;
+  return (
+    <Container>
+      <h1>Admin</h1>
+      <div>Is 1 even: {getIsEventText(7)}</div>
+      <div>Is 2 even: {getIsEventText(2)}</div>
+    </Container>
+  );
 };
