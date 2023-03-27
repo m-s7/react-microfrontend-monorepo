154c26291e33d6541269eb2eb79da5a89392d514
Michał Smolik <smolik.it@gmail.com>
2023-03-27 23:32:53 +0200

    update

---
+++ apps/shell/src/pages/settings/[remote].tsx
@@ -0,0 +1,21 @@
+import React, { useContext } from 'react';
+import dynamic from 'next/dynamic';
+import { Layout } from '../../layouts';
+import { ShellContextProps } from 'shared-context';
+import { AuthContext } from '../../business';
+
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
+const SettingsRemote = dynamic<ShellContextProps>(import('settings/Settings/General'), { ssr: false });
+
+export const Settings = () => {
+  const { authToken } = useContext(AuthContext);
+
+  return <SettingsRemote authToken={authToken} />;
+};
+
+Settings.getLayout = function getLayout(page: React.ReactElement) {
+  return <Layout>{page}</Layout>;
+};
+
+export default Settings;
